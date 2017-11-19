19eb9a2f07e976f309ad05e2de96928322486839
Bill <bill.gryta@gmail.com>
2017-11-19 09:42:11 -0600

    babel-installed

--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,9 +1,13 @@
-function Person(fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function() {
+class Person {
+  constructor (fullName, favColor){
+    this.name = fullName;
+    this.favoriteColor = favColor;
+  }
+
+  greet()  {
     console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
   }
 }
 
-module.exports = Person;
+// module.exports = Person;
+export default Person;
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ module.exports = {
  output: {
  path: __dirname +  "/app/temp/scripts",
  filename: "App.js"
- }
+},
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
 
 }
