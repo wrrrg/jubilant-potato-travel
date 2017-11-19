902a76972b9ab31a05575ce0604f2cca2046dd44
BillMac <bill.gryta@gmail.com>
2017-11-18 22:55:28 -0600

    watchjavascriptworks

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
-  output: {
-    path: "./app/temp/scripts",
-    filename: "App.js"
-  }
+ entry: "./app/assets/scripts/App.js",
+ output: {
+ path: __dirname +  "/app/temp/scripts",
+ filename: "App.js"
+ }
+
 }
