b959da432783bfbbf64eaa3ea5313d6f4e2eb9ba
Bill <bill.gryta@gmail.com>
2017-11-18 18:28:32 -0600

    working-gulp-complete-testimonials

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+  svgSprite = require('gulp-svg-sprite');
+
+var config = {
+  mode: {
+    css: {
+      render: {
+        css: {
+          template: './gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
+}
+
+  gulp.task('createSprite', function(){
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+      .pipe(svgSprite(config))
+      .pipe(gulp.dest('./app/temp/sprite/'));
+  });
+
+  gulp.task('copySpriteCSS', function() {
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+
+    });
