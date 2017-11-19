30e3692b379688184832e0acf02ea5863bca16e2
Bill <bill.gryta@gmail.com>
2017-11-19 10:15:12 -0600

    menu-done-before-hamburger

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
-
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
